d9c8a0068848b62abad80576d58f649ff2fffeda
zhouzhangqi <1978892854@qq.com>
2019-06-15 22:09:01 +0800

    递归完成

---
+++ src/com/zzq/recursion/LinkedListRecursion.java
@@ -0,0 +1,7 @@
+package com.zzq.recursion;
+
+/**
+ * 对于链表的增删改查进行递归的实现
+ */
+public class LinkedListRecursion {
+}
--- src/com/zzq/recursion/Sum.java
+++ src/com/zzq/recursion/Sum.java
@@ -13,6 +13,7 @@ public class Sum {
 
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6,7,8};
+        System.out.println(nums.length);
         System.out.println(sum(nums));
     }
 
