d45dd42e501e15f4185280ea91c4c95988388f0b
zhouzhangqi <1978892854@qq.com>
2019-06-22 20:58:18 +0800

    leetcode解题

--- src/com/zzq/BST/BST.java
+++ src/com/zzq/BST/BST.java
@@ -1,5 +1,7 @@
 package com.zzq.BST;
 
+import java.util.Stack;
+
 /**
  * 实现二分搜索树
  */
@@ -27,30 +29,134 @@ public class BST<E extends Comparable<E>> {
     }
     //向二分搜索树中添加新的元素
     public void add(E e){
-        if (root == null){
-            root = new Node(e);
-            size ++;
-        }
-        else
-            add(root , e);
+       root = add(root,e);
     }
     //向以node为根的二分搜索树中插入元素e,递归算法
-    private void add(Node node , E e){
-        if (e.equals(node.e))
-            return;
-        else if (e.compareTo(node.e) < 0 && node.left == null){
-            node.left = new Node(e);
+    //返回
+    private Node add(Node node , E e){
+        if (node == null){
             size ++;
-            return;
-        }
-        else if (e.compareTo(node.e) > 0 && node.right == null){
-            node.right = new Node(e);
-            size ++;
-            return;
+            return new Node(e);
         }
         if (e.compareTo(node.e) < 0)
-            add(node.left,e);
+            node.left = add(node.left,e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right,e);
+        return node;
+    }
+    //查看二分搜索树中是否包含e
+    public boolean contains(E e){
+        return contains(root,e);
+    }
+    //看以node为根的二分搜索树是否包含元素e,递归算法
+    private boolean contains(Node node ,E e){
+        if (node == null)
+            return false;
+        if (e.compareTo(node.e) == 0)
+            return true;
+        else if (e.compareTo(node.e) < 0)
+            return contains(node.left,e);
         else
-            add(node.right,e);
+            return contains(node.right,e);
+    }
+    //二分搜索树的前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
+    //前序遍历以node为根的二分搜索树，递归算法
+    private void preOrder(Node node){
+        if (node == null)
+            return;
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    //二分搜索树的非递归前序遍历
+    public void preOrderNR(){
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+            if (cur.right != null)
+                stack.push(cur.right);
+            if (cur.left != null)
+                stack.push(cur.left);
+        }
+    }
+    //二分搜索树的中序遍历,中序遍历是顺序排列的
+    public void inOrder(){
+        inOrder(root);
+    }
+    //中序遍历以node为根的二分搜索树，递归算法
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+    //二分搜索树的后序遍历
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root,0,res);
+        return res.toString();
+    }
+
+    private void generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null){
+            res.append(generateDepthString(depth)+"null\n");
+            return;
+        }
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left,depth+1,res);
+        generateBSTString(node.right,depth + 1 ,res);
+
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth ; i++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5,3,6,8,4,2};
+        for (int num : nums) {
+            bst.add(num);
+        }
+        /////////////////////
+        //     5          //
+        //  3     6      //
+        //2    4     8  //
+        //               //
+        ///////////////////
+        bst.preOrder();
+        System.out.println();
+        bst.preOrderNR();
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
+        System.out.println();
+       /* System.out.println();
+        System.out.println(bst);*/
     }
 }
